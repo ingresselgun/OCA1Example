9b968ca26432a4d9b720e8ee88452f932f575813
ingresselgun <ingresselgun010@gmail.com>
2023-12-08 18:39:43 +0400

    new beanches

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("50000 setr kod var buda ");
+        System.out.println("50000 setr kod var buda");
+        System.out.println("added new two branch registr login");
     }
 }
-//
+
